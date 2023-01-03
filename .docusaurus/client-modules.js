7e10731e958d8554566bdd332c843146acd1da1d
Nuro <82734446+NuroC@users.noreply.github.com>
2023-01-03 08:43:16 -0500

    init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/nuro/Documents/GitHub/cybersecurity-information/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/nuro/Documents/GitHub/cybersecurity-information/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/nuro/Documents/GitHub/cybersecurity-information/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/nuro/Documents/GitHub/cybersecurity-information/src/css/custom.css'),
+];
